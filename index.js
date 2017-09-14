74ab667e2edb692a00d6be6444362b5cef4640a5
Sara Hopper <sara.hopper@gmail.com>
2017-09-14 17:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'cc8eb717d6bb9cf3fcad1ecad094dbe540eff761'
+  return '760869047ff1c29982d033c65ce549b346dfeebc'
 }
