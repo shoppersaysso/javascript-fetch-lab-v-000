af39c3b48a7dc078e93375b7d7b79bf7344b6953
Sara Hopper <sara.hopper@gmail.com>
2017-09-14 16:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function createIssue() {
 
 function showResults(results) {
   const repo = 'https://api.github.com/repos/shoppersaysso/javascript-fetch-lab/issues'
-  var forks = JSON.parse(this.responseText)
   const forkList = "<ul>" + results.map(fork => {
     return (`
       <li>Title: <a href="${this.url}">${this.title} </a> — Body: ${this.body}</li>
