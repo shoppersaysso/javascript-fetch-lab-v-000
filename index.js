98b0bbf7ffa63bb301797c44115b5cf5be142d69
Sara Hopper <sara.hopper@gmail.com>
2017-09-14 17:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+function getIssues() {
+  const repo = 'https://api.github.com/repos/shoppersaysso/javascript-fetch-lab/issues'
+  fetch(repo, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${token}`,
+    }
+  }).then(res => showIssues(res));
 
-
-// function getIssues() {
-//   const repo = 'https://api.github.com/repos/shoppersaysso/javascript-fetch-lab/issues'
-//   fetch(repo, {
-//     method: 'get',
-//     headers: {
-//       Authorization: `token ${token}`,
-//     }
-//   }).then(res => showIssues(res));
-//
-// }
+}
 
 function showIssues(json) {
   var div = document.getElementById("issues")
