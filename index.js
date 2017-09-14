c7125b2d8da3c64bce25c320ffe0a9b6662301cd
Sara Hopper <sara.hopper@gmail.com>
2017-09-14 17:14:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function createIssue() {
 
 function showResults(json) {
   // $('#results').append(`<a href="${json.html_url}"> ${json.html_url}</a>`)
-  $('#results').append(`<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>)`)
+  $('#results').append(`<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`)
 }
 
 function forkRepo() {
