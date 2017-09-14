e869608e3e1b6e24bb4337a35305048fa93c418f
Sara Hopper <sara.hopper@gmail.com>
2017-09-14 15:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const token = '760869047ff1c29982d033c65ce549b346dfeebc';
-// const postData = {
-//   body: 'Great stuff'
-// };
-//
+const token = '760869047ff1c29982d033c65ce549b346dfeebc';
+const postData = {
+  body: 'Great stuff'
+};
+
 // fetch('https://api.github.com/repos/shoppersaysso/space-invaders-v-000/commits/2e6c34c649a5d091f32d72d2ee2011bb64bd7823/comments', {
 //   method: 'POST',
 //   body: JSON.stringify(postData),
@@ -27,7 +27,7 @@ function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch('https://api.github.com/repos/' + repo + '/forks',{
     method: 'POST',
-    body: JSON.stringify()
+    body: JSON.stringify(postData)
   }
 
 
